f747fb817b09710244c500637b1e5ad658da2935
Brian Ball <ideabrian@gmail.com>
2014-11-25 22:46:48 -0800

    Add security by moving collection calls to methods that will be run on the server.

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -27,12 +27,15 @@ if (Meteor.isClient) {
 
       var text = event.target.text.value;
 
-      Tasks.insert({
-        text: text,
-        createdAt: new Date(), // current time
-        owner: Meteor.userId(),
-        username: Meteor.user().username
-      });//insert
+      // Tasks.insert({
+      //   text: text,
+      //   createdAt: new Date(), // current time
+      //   owner: Meteor.userId(),
+      //   username: Meteor.user().username
+      // }); old insert
+
+      //insert
+      Meteor.call("addTask", text);
 
       // Clear form
       event.target.text.value = "";
@@ -47,10 +50,17 @@ if (Meteor.isClient) {
 
   Template.task.events({ // add an event handler
     "click .toggle-checked": function() {
-      Tasks.update(this._id, {$set: {checked: ! this.checked}});
+      // Tasks.update(this._id, {$set: {checked: ! this.checked}}); // old Tasks.update
+
+      // update Tasks
+      Meteor.call("setChecked", this._id, ! this.checked);
+
     },
     "click .delete": function() {
-      Tasks.remove(this._id);
+      // Tasks.remove(this._id); // old remove
+
+      // remove
+      Meteor.call("deleteTask", this._id);
     }
   });
 
@@ -58,3 +68,25 @@ if (Meteor.isClient) {
     passwordSignupFields: "USERNAME_ONLY"
   });
 }
+
+Meteor.methods({
+  addTask: function (text) {
+    // Make sure the user is logged in before inserting a task
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Tasks.insert({
+      text: text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+  deleteTask: function (taskId) {
+    Tasks.remove(taskId);
+  },
+  setChecked: function (taskId, setChecked) {
+    Tasks.update(taskId, { $set: { checked: setChecked} });
+  }
+});
